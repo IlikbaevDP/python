53fffbdf7f87ccb1535af9134e9fbe0ad3fb9576
IlikbaevDP <IlikbaevDP@mail.ru>
2025-04-29 23:02:39 +0500

    myList Update

--- my.py
+++ my.py
@@ -3,9 +3,24 @@ print("Deins")
 print("вторая строка")
 
 print("третья строка")
-print("четвертая строка")
 
-# множества (sets)
+print(ord("A"))
+print(f"\\u{ord("a")}")
+# Оператор
+a = 5
+b = 10
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(8%2)
+print(a**2)
+print(-10//3)
 
-myList = ["Добрый", "добрый", "день", 'день']
-
+print(5 ==5)
+a += a + b
+print(a)
+a +=b
+print(a)
+print("Dion")
