c8fc5fb682cf51c91fb7107cdd9d96800fc4bf00
IlikbaevDP <IlikbaevDP@mail.ru>
2025-05-03 23:28:46 +0500

    DZ myList Update

--- Dz.py
+++ Dz.py
@@ -1,4 +1,4 @@
- # Домашняя работа 2
+from http.cookiejar import user_domain_match  # Домашняя работа 2
 print("Denis dz  20.04")
 print (2**5)
 a = 32
@@ -11,3 +11,13 @@ e = 126
 f = -15
 result = e-f
 print(e, "-", f, "=", result)
+
+# Домашняя работа 3 от 26.04.2025
+print("Как тебя зовут")
+name = input()
+print('Привет,', name)
+print('Сколько тебе лет?')
+age =input()
+print('Ты любишь программирование да / нет?')
+user_input = input('Ты любишь программирование (да / нет): ')
+yes_choices = ['yes', 'y']
