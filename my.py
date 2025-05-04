cc1fbf8d3fc57a140ad67c7b46417f24d6a4aa71
IlikbaevDP <IlikbaevDP@mail.ru>
2025-05-04 16:35:41 +0500

    DZ

--- my.py
+++ my.py
@@ -4,23 +4,28 @@ print("вторая строка")
 
 print("третья строка")
 
-print(ord("A"))
-print(f"\\u{ord("a")}")
-# Оператор
+print("Привет", 'мир', '!')
+
+print (55, 67, 78.5, True,False )
+
 a = 5
 b = 10
 print(a+b)
-print(a-b)
-print(a*b)
-print(a/b)
-print(a%b)
-print(8%2)
-print(a**2)
-print(-10//3)
+print("5 + 10 =", 5 + 10)
 
-print(5 ==5)
-a += a + b
+print("4", "5", "6", "7", sep="-")
+a = 17
 print(a)
-a +=b
+a = b = f = 7
+print(a, b, f)
+
+a -= 1
+a = a - 1
 print(a)
-print("Dionid")
+
+print(5/2)
+print(5//2)
+
+print(5 * 2)
+print(5**2)
+
