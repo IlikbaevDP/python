736118922bae4b5dffdf2c6788c40e00024a1407
IlikbaevDP <IlikbaevDP@mail.ru>
2025-05-04 20:30:43 +0500

    DZ 27.04.2025

--- dz2.py
+++ dz2.py
@@ -1,4 +1,5 @@
 # ДЗ_QA415_26.04  задание 2 (y > x - 2)
+
 y = 3
 x = 5
 print (y > x - 2)
@@ -6,3 +7,12 @@ print (y > x - 2)
 y = 5
 x = 3
 print(y > x - 2)
+
+# ДЗ_ QA415_27.04
+#Gets user input
+name = input("What is your name?")
+age = input("how old are you?")
+user_input = input('Do you like programming (yes/no): ')
+if user_input.lower() == 'yes':
+    print('user typed yes')
+# словари
